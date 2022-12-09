27b053b8ca363d23827fd989c4d705c371beecee
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2022-12-10 01:16:19 +0900

    second commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-All notable changes to the "git-diff-monitor" extension will be documented in this file.
+All notable changes to the "git-diff-alert" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,43 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    'git-diff-monitor.helloWorld',
+  vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+		runGitDiff();
+	});
+  vscode.commands.registerCommand(
+    'git-diff-alert.run',
     () => {
-      vscode.window.showInformationMessage('HogeHoge');
+      runGitDiff();
     }
-  );
-
-  context.subscriptions.push(disposable);
-  const button = vscode.window.createStatusBarItem(
-     vscode.StatusBarAlignment.Right, 
-     0
-   );
-   button.command = 'git-diff-monitor.omikuji';
-   button.text = 'おみくじを引く';
-   context.subscriptions.push(button);
-   button.show();
+  ); 
 }
+
+const runGitDiff = () => {
+  const config = vscode.workspace.getConfiguration('gitDiffalert');
+  const threshold = config.get<number>('threshold') ?? 100;
+  if (!config.get<number>('threshold')){
+    vscode.window.showInformationMessage("Threshold is changed to 100.");
+  }
+  const baseBranch = config.get<"string">('baseBranch') ?? "main";
+  if (!config.get<"string">('baseBranch') ){
+    vscode.window.showInformationMessage("base branch is changed to main.");
+  }
+  const execSync = require('child_process').execSync;
+  const cmd = `git diff ${baseBranch} --numstat | awk '{ add+=$1; del+=$2 } END { print add+del }'`;
+  const result = execSync(cmd).toString().split(',');
+  if (!result.length || result.length > 1){
+    vscode.window.showErrorMessage("Error occured by running git diff.");
+    return;
+  }
+  const diff = result[0].replace("\n","");
+  const parsedDiff = parseInt(diff, 10);
+  if (isNaN(parsedDiff)) { 
+    vscode.window.showErrorMessage("Error occured by running git diff.");
+    return;
+  }
+  if (Number(diff) > threshold){
+    vscode.window.showWarningMessage(`The total of added lines and deleted lines is ${diff}. It is over ${threshold}.`);
+  }
+};
 
 export function deactivate() {}
